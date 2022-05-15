11d0ee3bee22c8399afe76f6730cb77344a184a6
Shrisha Shrestha <shrishashrestha101@gmail.com>
2022-05-15 12:00:05 +0545

    Learnng to make a ID Card

--- lib/main.dart
+++ lib/main.dart
@@ -1,135 +1,98 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MaterialApp(
-      home: lesson4(),
-    ));
+void main() => runApp(MaterialApp(
+home: IDcard(),
+));
 
-class lesson4 extends StatelessWidget {
-  const lesson4({Key? key}) : super(key: key);
+class IDcard extends StatelessWidget {
+  const IDcard({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black54,
       appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: Text(
-          'MYAPP',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 25.0,
-          ),
-        ),
+        backgroundColor: Color.fromRGBO(31,31,31,1.0),
+        title: Text('Student ID Card'),
         centerTitle: true,
       ),
-      body: Padding(//IMPORTANT
-        padding: const EdgeInsets.all(18.0),
-        child: Column(
-          children: <Widget>[
-            Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-
-              children: <Widget>[
-                Icon(Icons.mail,
-                color: Colors.black,
-                  size: 50.0,
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Center(
+                child: CircleAvatar(
+                  backgroundImage: AssetImage('assets/600.jfif'),
+                  radius: 60.0,
                 ),
-
-                TextButton(
-                    onPressed:(){print('hi');},
-                    child: Text('Click'),
-                  style: TextButton.styleFrom(
-                      primary: Colors.white,
-                    backgroundColor: Colors.black,
-                    elevation: 20.0,
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-                    shadowColor: Colors.yellow,
-                ),
-                ),
-                SizedBox(width:5.0  ),
-
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.black,
-                    onPrimary: Colors.white,
-                    shadowColor: Colors.red,
-                    elevation: 20.0,
-                    padding: EdgeInsets.all(2.0)
-                  ),
-                  onPressed:(){print('hi');},
-                    child: Text('Click'),
-                ),
-                SizedBox(width:5.0  ),
-                ElevatedButton.icon(
-                  style: ElevatedButton.styleFrom(
-                      primary: Colors.black,
-                      onPrimary: Colors.white,
-                      shadowColor: Colors.lightGreen,
-                      elevation: 20.0,
-                      padding: EdgeInsets.all(5.0)
-                  ),
-                  icon: Icon(Icons.add),
-                  label: Text('Add'),
-                  onPressed:(){print('hi');},
-                ),
-
-                SizedBox(width:5.0  ),
-                Ink(
-                  //padding: EdgeInsets.all(20.0),
-                    decoration: const ShapeDecoration(
-                      color: Colors.black,
-                      shape: CircleBorder(),
-                    ),
-
-                child:IconButton(
-                    onPressed:(){print('hi');},
-                  icon: Icon(Icons.add),
-                    color: Colors.white,
-
-                ),
-                ), ]
-                ),
-              Expanded(
-                child: Image.asset('assets/600.jfif'),
               ),
-           Expanded(
-               flex: 1,
-               child: Container(
-             color: Colors.black,
-           )),
-            Expanded(
-                flex: 2,
-                child: Container(
-                  color: Colors.black87,
-                )),
-
-           Center(
-             child:Container(
-               //IMPORTANT
-               margin: EdgeInsets.all(5.0),
-               padding:EdgeInsets.all(10.0),
-               color: Colors.black,
-               child: Image.asset(
-                 'assets/600.jfif',
-                 height: 280.0,
-                 width: 300.0,
-               ),
-             ),
-           ),
-
-
-
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.black,
-        onPressed: () {print('Hello');},
-        child: Text(
-          'HI',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 10.0,
+              SizedBox(height: 20.0),
+              Divider(
+                height: 14.0,
+                color: Colors.grey,
+              ),
+              SizedBox(height: 20.0),
+              Text('NAME',
+              style: TextStyle(
+                fontSize: 15.0,
+                letterSpacing: 2.0,
+                color: Colors.grey,
+              ),),
+              SizedBox(height: 20.0),
+              Text('Shrisha Shrestha',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  letterSpacing: 2.0,
+                  color: Colors.white,
+                ),),
+              SizedBox(height: 20.0),
+              Text('ADDRESS',
+                style: TextStyle(
+                  fontSize: 15.0,
+                  letterSpacing: 2.0,
+                  color: Colors.grey,
+                ),),
+              SizedBox(height: 20.0),
+              Text('Madhyapur Thimi - 7',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  letterSpacing: 2.0,
+                  color: Colors.white,
+                ),),
+              SizedBox(height: 20.0),
+              Text('DATE OF BIRTH',
+                style: TextStyle(
+                  fontSize: 15.0,
+                  letterSpacing: 2.0,
+                  color: Colors.grey,
+                ),),
+              SizedBox(height: 20.0),
+              Text('2000 - 01 - 02',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  letterSpacing: 2.0,
+                  color: Colors.white,
+                ),),
+              SizedBox(height: 20.0),
+              Row(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Icon(Icons.mail,
+                    color:Colors.white),
+                  ),
+                  Text('shrishashrestha101@gmail.com',
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      letterSpacing: 2.0,
+                      color: Colors.white,
+                    ),),
+                ],
+              )
+            ],
           ),
         ),
       ),
